60bd8c876f9ab6160abae02c6cd74615746de2cd
Malte Schlüter <malte.schlueter@qossmic.com>
2021-06-04 15:37:18 +0200

    Use php attributes

--- src/Command/AppFetchCoronaCasesCommand.php
+++ src/Command/AppFetchCoronaCasesCommand.php
@@ -5,23 +5,27 @@ declare(strict_types=1);
 namespace App\Command;
 
 use App\Scraper\Scraper;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Serializer\Encoder\JsonEncode;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
 
+#[AsCommand(
+    name: 'app:fetch-corona-cases'
+)]
 final class AppFetchCoronaCasesCommand extends Command
 {
-    protected static $defaultName = 'app:fetch-corona-cases';
-
     /**
      * @param Scraper[] $scraper
      */
     public function __construct(
+        #[TaggedIterator('app.scraper')]
         private iterable $scraper,
         private SerializerInterface $serializer,
         private string $dataPath
@@ -36,6 +40,8 @@ final class AppFetchCoronaCasesCommand extends Command
         $filesystem = new Filesystem();
 
         foreach ($this->scraper as $scraper) {
+            $io->info(sprintf('Fetching "%s"', $scraper->getId()));
+
             $data = $scraper->scrap();
 
             $jsonData = $this->serializer->serialize(
--- src/Dto/Data.php
+++ src/Dto/Data.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Dto;
 
+use Symfony\Component\Serializer\Annotation as Serializer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+
 final class Data
 {
     /**
@@ -13,6 +16,7 @@ final class Data
 
     public function __construct(
         public string $source,
+        #[Serializer\Context(normalizationContext: [DateTimeNormalizer::FORMAT_KEY => 'Y-m-d'])]
         public \DateTimeImmutable $prDate
     ) {
     }
